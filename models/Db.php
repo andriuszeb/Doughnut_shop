838a5e53c6686f37e9dbe3d6c6e5d2881e1ff52f
Andrius Zebrauskas <[zebrauskas.andrius@gmail.com]>
2022-09-30 21:28:59 +0300

    pirma dalis

---
+++ models/Db.php
@@ -0,0 +1,18 @@
+<?php
+
+class Db {
+    public $conn;
+
+    public function __construct(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "shop";
+        $this->conn = new mysqli($servername,$username,$password,$db);
+        
+    }
+    
+
+}
+
+?>
